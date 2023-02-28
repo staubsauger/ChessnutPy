735ea883c0b6819eb9e95807b18d0a3be4cf6ca5
Lukas Nöllemeyer <babelfisch92@gmx.de>
2023-02-28 16:20:41 +0100

    refactored things out of the basic API

--- ChessnutAir.py
+++ ChessnutAir.py
@@ -14,22 +14,17 @@ from constants import DEVICELIST
 import data2fen
 from setledhelpers import turnOffat, turnOnat
 
+
 class ChessnutAir:
-    """Class created to discover chessnut Air devices.
-    It returns the first device with a name that maches
-    the names in DEVICELIST.
+    """Class created to discover and connect to chessnut Air devices.
+    It discovers the first device with a name that maches the names in DEVICELIST.
     """
-    def __init__(self, timeout=10.0):
-        self.deviceNameList = DEVICELIST # valid device name list
+    def __init__(self):
+        self.deviceNameList = DEVICELIST  # valid device name list
         self.device = self.advertisement_data = self.connection = None
-        self.running = False
+        self.boardstate = [0]*32
         self.old_data = [0]*32
-        self.boardstate = ""
-        self.onledsarr = [0x0A, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
-        self.onledstack = []
-        self.tick = 1
-        self.to_blink = [["e", "4"]]
-        self.to_light = [["e", "2"]]
+        self.led_command = [0x0A, 0x08]
 
     def filter_by_name(
         self,
@@ -40,7 +35,7 @@ class ChessnutAir:
         return True if the device name is in the list of 
         valid device names otherwise it returns False"""
         if any(ext in device.name for ext in self.deviceNameList):
-            self.device = device            
+            self.device = device
             return True
         return False
 
@@ -54,76 +49,48 @@ class ChessnutAir:
             return
         print("done scanning")
 
-    def returnboardstate(self):
+    def boardstate_fen(self):
         fen = data2fen.get_fen(self.boardstate)
-        print(fen)
+        # print(fen)
         return fen
 
     async def piece_up(self, location, id):
         raise NotImplementedError
 
-    async def piece_down(self, location, id): # location -> pos: x = location%8, y = location//8
+    async def piece_down(self, location, id):  # location -> pos: x = location%8, y = location//8
         raise NotImplementedError
-    
-    async def showmoveonboard(self, bytearr):
+
+    async def game_loop(self):
         raise NotImplementedError
-    
-    def add_square_to_leds(self, move1, move2):
-        if move1 + move2 not in self.onledstack:
-            # print("nicht drin")
-            self.onledsarr = turnOnat(move1, move2, self.onledsarr)
-            self.onledstack.append(move1+move2)
 
-    def rem_square_from_leds(self, move1, move2):
-        if move1 + move2 not in self.onledstack:
-            print("nicht drin, kann nicht ausschalten")
-        else:
-            self.onledsarr = turnOffat(move1, move2, self.onledsarr)
-            self.onledstack.remove(move1+move2)
-
-    def blink_on(self, move1, move2):
-        if self.tick == 1:
-            self.add_square_to_leds(move1, move2)
-        else:
-            self.rem_square_from_leds(move1, move2)
-
-    def turnLedOnOn(self, square):
-        # square like "e2"
-        if [square[0], square[1]] not in self.to_light:
-            self.to_light.append([square[0], square[1]])
-        print("on", self.to_light)
-
-    def turnLefOffOn():
-        pass
-
-    def blinkLedOn(self, square):
-        self.to_blink.append([square[0], square[1]])
-        
-    def blinkLedOff():
-        pass
+    async def change_leds(self, list_of_pos):
+        """Turns on all LEDs in list_of_pos and turns off all others.
+            list_of_pos := ["e3", "a4",...]"""
+        conv_letter = {"a": 128, "b": 64, "c": 32, "d": 16, "e": 8, "f": 4, "g": 2, "h": 1}
+        conv_number = {"1": 7, "2": 6, "3": 5, "4": 4, "5": 3, "6": 2, "7": 1, "8": 0}
+        arr = bytearray([0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
+        for pos in list_of_pos:
+            arr[conv_number[pos[1]]] += conv_letter[pos[0]]
+        await self.connection.write_gatt_char(WRITECHARACTERISTICS, self.led_command + arr)
 
     async def handler(self, char, data):
-        self.boardstate = data[2:34]
+        async def send_message(loc, old, new):
+            if old != new:
+                if new == 0:
+                    await self.piece_up(loc, old)
+                else:
+                    await self.piece_down(loc, new)
         rdata = data[2:34]
         if rdata != self.old_data:
+            self.boardstate = rdata
             for i in range(32):
                 if rdata[i] != self.old_data[i]:
-                    clow = rdata[i] & 0xf
-                    olow = self.old_data[i] & 0xf
-                    if clow != olow:
-                        if clow == 0:
-                            await self.piece_up(i*2, olow)
-                            print("clow == 0")
-                        else:
-                            await self.piece_down(i*2, clow)
-                            print("clow != 0")
-                    else:
-                        if rdata[i] >> 4 == 0:
-                            await self.piece_up(i*2+1, self.old_data[i] >> 4)
-                            print("rdata[i] >> 4 == 0")
-                        else:
-                            await self.piece_down(i*2+1, rdata[i] >> 4)
-                            print("rdata[i] >> 4 != 0")
+                    cur_left = rdata[i] & 0xf
+                    old_left = self.old_data[i] & 0xf
+                    await send_message(i*2, old_left, cur_left)
+                    cur_right = rdata[i] >> 4
+                    old_right = self.old_data[i] >> 4
+                    await send_message(i*2+1, old_right, cur_right)
             self.old_data = rdata
 
     async def run(self, debug=False):
@@ -133,23 +100,10 @@ class ChessnutAir:
         async with BleakClient(self.device) as client:
             self.connection = client
             print(f"Connected: {client.is_connected}")
-            self.running = True
             # send initialisation string
             await client.write_gatt_char(WRITECHARACTERISTICS, INITIALIZASION_CODE)  # send initialisation string
             print("Initialized")
             await client.start_notify(READDATA, self.handler)  # start another notification handler
-            while self.running:
-                
-                if self.tick == 1:
-                    self.tick = 0
-                else:
-                    self.tick = 1
-                print("Tick: ", self.tick)
-                for i in self.to_light:
-                    self.add_square_to_leds(i[0], i[1])
-                for i in self.to_blink:
-                    self.blink_on(i[0], i[1])
-                await self.showmoveonboard(self.onledsarr)
-                await asyncio.sleep(1)
+            await self.game_loop()  # call user game loop
             await client.stop_notify(READDATA)  # stop the notification handler
 
--- main.py
+++ main.py
@@ -4,6 +4,13 @@ from ChessnutAir import ChessnutAir
 from GameOfChess import GameOfChess
 
 class Board(ChessnutAir):
+    def __init__(self):
+        super.__init__()
+        self.running = False
+        self.boardstate = ""
+        self.tick = False
+        self.to_blink = ["a3", "b4", "c7", "h3"]
+
     async def piece_down(self, location, id):
         print(f"piece: {convertDict[id]} at {location} down")
         # print(self.returnboardstate())
@@ -12,9 +19,17 @@ class Board(ChessnutAir):
 
     async def piece_up(self, location, id):
         print(f"piece: {convertDict[id]} at {location} up")
-        
-    async def showmoveonboard(self, bytearr):
-        await self.connection.write_gatt_char(WRITECHARACTERISTICS, bytearr)
+
+    async def game_loop(self):
+        self.running = True
+        while self.running:
+            self.tick = not self.tick
+            #print("Tick: ", self.tick)
+            if self.tick:
+                await self.change_leds(self.to_blink)
+            else:
+                await self.change_leds([])
+            await asyncio.sleep(1)
 
 async def go():
     b = Board()
